3b7be32da1e0f65a9ac3388ad4db2fec8d3ba1a6
skfkgla <narahim.lee@gmail.com>
2026-01-06 18:10:02 +0900

    디자인 변경 - Service -> Project

--- site.config.js
+++ site.config.js
@@ -13,6 +13,14 @@ const CONFIG = {
       name: `SSaG - 패션 최저가 검색 서비스`,
       href: "https://github.com/nbc-expert-6/SSaG",
     },
+    {
+      name: `ShoongLogistics - B2B 물류 관리 서비스`,
+      href: "https://github.com/nbc-expert-6/ShoongLogistics",
+    },
+    {
+      name: `Turkey - B2C 음식 배달 서비스`,
+      href: "https://github.com/turkey-team/delivery-service-backend",
+    },
   ],
   // blog setting (required)
   blog: {

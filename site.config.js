bdd20fd75c3161519f786e4bdfc888f2d4d7dde1
skfkgla <narahim.lee@gmail.com>
2026-01-06 15:52:09 +0900

    블로그 변경사항 적용

--- site.config.js
+++ site.config.js
@@ -1,32 +1,31 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    name: "narahim",
+    image: "/my-notion-face-portrait.png",
+    role: "Backend Developer",
+    bio: "데이터 기반 의사결정으로 문제를 해결하는 백엔드 개발자",
+    email: "narahim.lee@gmail.com",
     linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    github: "https://github.com/skfkgla",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `SSaG - 패션 최저가 검색 서비스`,
+      href: "https://github.com/nbc-expert-6/SSaG",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "기록의 him",
+    description: "백엔드 개발 이야기",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://morethan-log-54sw.vercel.app/",
+  since: 2026, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)

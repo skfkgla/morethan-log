8b6ab5b5695ce2d7763bdf565eb4fe588f6068b0
skfkgla <narahim.lee@gmail.com>
2026-01-06 16:37:36 +0900

    오류 해결

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   profile: {
     name: "narahim",
-    image: "/my-notion-face-portrait.png",
+    image: "/avartar.svg",
     role: "Backend Developer",
     bio: "데이터 기반 의사결정으로 문제를 해결하는 백엔드 개발자",
     email: "narahim.lee@gmail.com",
